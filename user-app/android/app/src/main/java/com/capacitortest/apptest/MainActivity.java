1eaad7cb22321eba1fa7fb6fa58ef257279e19a1
Mouayed Keziz <91923112+mouayed-keziz@users.noreply.github.com>
2022-12-02 00:23:13 +0000

    init repository with capacitor

---
+++ user-app/android/app/src/main/java/com/capacitortest/apptest/MainActivity.java
@@ -0,0 +1,5 @@
+package com.capacitortest.apptest;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ user-app/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.capacitortest.apptest',
+  appName: 'app-test',
+  webDir: 'build',
+  bundledWebRuntime: false,
+};
+
+export default config;
